51fec99cc59a9a3f3cec172cb1a584d5491f36ff
Ji2z <leegin98@naver.com>
2020-05-13 17:05:24 +0900

    회원가입 통신 연결 중

--- app/src/main/java/com/example/jms/etc/SignUp.java
+++ app/src/main/java/com/example/jms/etc/SignUp.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.jms.R;
+import com.example.jms.connection.viewmodel.APIViewModel;
+import com.example.jms.settings.PasswordFind1;
 
 public class SignUp extends AppCompatActivity {
 
@@ -18,16 +21,20 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
 
-        /*Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_arrow2_back_24dp);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });*/
+        EditText txtName = (EditText)findViewById(R.id.name);
+        EditText txtBirth = (EditText)findViewById(R.id.birth);
+        EditText txtEmail = (EditText)findViewById(R.id.email);
+        EditText txtPassword = (EditText)findViewById(R.id.password);
 
+        String name = txtName.getText().toString();
+        String birth = txtBirth.getText().toString();
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
 
-
+        APIViewModel apiViewModel = new APIViewModel();
+    }
+    public void goLogin(View view){
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
     }
 }
