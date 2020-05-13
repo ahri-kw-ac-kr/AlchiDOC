eb477fe72ffd4497f2a6163bb9ba94e8b8e8d199
Ji2z <leegin98@naver.com>
2020-05-14 02:31:39 +0900

    현위치 수정

--- app/src/main/java/com/example/jms/map/FragMap.java
+++ app/src/main/java/com/example/jms/map/FragMap.java
@@ -7,21 +7,83 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.jms.R;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class FragMap extends Fragment {
-
+public class FragMap extends Fragment implements OnMapReadyCallback{
     private View view;
-
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_map, container, false);
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(35.1798159, 129.0750222), // 대상 지점
+                16, // 줌 레벨
+                20, // 기울임 각도
+                0 // 베어링 각도
+        );
+
+        NaverMapOptions options = new NaverMapOptions()
+                .camera(cameraPosition)
+                .mapType(NaverMap.MapType.Terrain)
+                .enabledLayerGroups(NaverMap.LAYER_GROUP_BUILDING)
+                .compassEnabled(true)
+                .scaleBarEnabled(true);
+
+        /*FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance(options);
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);*/
+
+        MapView mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
         return view;
-
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+    }
+
+
 }
