ca1434d4f434f748bfd096f61dd3822f97523157
Ji2z <leegin98@naver.com>
2020-05-21 02:44:20 +0900

    팝업 위치 수정

--- app/src/main/java/com/example/jms/etc/IntroActivity.java
+++ app/src/main/java/com/example/jms/etc/IntroActivity.java
@@ -1,9 +1,14 @@
 package com.example.jms.etc;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.core.app.ActivityCompat;
 
 import com.example.jms.R;
 
@@ -20,8 +25,7 @@ public class IntroActivity extends Activity {
                 Intent intent = new Intent (getApplicationContext(), Login.class);
                 startActivity(intent);
                 finish();
-            }
-        },1000); //1초 후에 메인화면 나타남(인트로 화면을 몇 초 동안 보여줄 것인지)
+            }},1000); //1초 후에 메인화면 나타남(인트로 화면을 몇 초 동안 보여줄 것인지)
     }
     @Override
     protected void onPause(){
