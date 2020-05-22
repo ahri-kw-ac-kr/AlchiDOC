6378b65592aea2d7a20d7de64bef7e4509ea96d8
tetris8010 <tetris8010@gmail.com>
2020-05-22 18:31:37 +0900

    apk 생성위해 수정

--- app/src/main/java/com/example/jms/settings/DeviceSet3.java
+++ app/src/main/java/com/example/jms/settings/DeviceSet3.java
@@ -1,16 +1,12 @@
 package com.example.jms.settings;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.jms.R;
 
@@ -19,6 +15,8 @@ public class DeviceSet3 extends LinearLayout {
     View view;
     TextView textView;
     Button button;
+
+    public DeviceSet3() {super(null);}
 
     public DeviceSet3(Context context, AttributeSet attributeSet){
         super(context,attributeSet);
@@ -29,10 +27,12 @@ public class DeviceSet3 extends LinearLayout {
         super(context);
         init(context);
     }
+
     private void init(Context context){
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(R.layout.device3, this, true);
         textView = (TextView)view.findViewById(R.id.deviceBattery);
         button = (Button)view.findViewById(R.id.deviceFinish);
     }
+
 }
